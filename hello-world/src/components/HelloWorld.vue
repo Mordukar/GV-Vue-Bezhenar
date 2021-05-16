415113b49844ee7014174c8e2c4092f17f7f75e8
Mordukar <t80_01@mail.ru>
2021-05-16 19:27:23 +0300

    Fix

--- hello-world/src/components/HelloWorld.vue
+++ hello-world/src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
-    <input v-model.number="operand1"/>
-    <input v-model.number="operand2"/>
+    <input v-model.number="operand1" />
+    <input v-model.number="operand2" />
     = {{ result }}
     <div>
       <button @click="calculate('+')">+</button>
@@ -10,26 +10,22 @@
       <button @click="calculate('/')">/</button>
       <button @click="calculate('^')">^</button>
     </div>
-<!--    <div>-->
-<!--      <input type="checkbox" id="checkbox" v-model="checked">-->
-<!--      <label for="checkbox">{{ checked }}</label>-->
-<!--    </div>-->
+    <!--    <div>-->
+    <!--      <input type="checkbox" id="checkbox" v-model="checked">-->
+    <!--      <label for="checkbox">{{ checked }}</label>-->
+    <!--    </div>-->
     <div>
-      <button
-        v-for="item in items"
-        :key="item"
-        @click="operand1 + item"
-      >
+      <button v-for="item in items" :key="item" @click="() => operand1 + item">
         {{ item }}
       </button>
     </div>
-<!--    <div>-->
-<!--      <input type="radio" id="one" value="Один" v-model="picked">-->
-<!--      <label for="one">Операнд 1</label>-->
-<!--      <br>-->
-<!--      <input type="radio" id="two" value="Два" v-model="picked">-->
-<!--      <label for="two">Операнд 2</label>-->
-<!--    </div>-->
+    <!--    <div>-->
+    <!--      <input type="radio" id="one" value="Один" v-model="picked">-->
+    <!--      <label for="one">Операнд 1</label>-->
+    <!--      <br>-->
+    <!--      <input type="radio" id="two" value="Два" v-model="picked">-->
+    <!--      <label for="two">Операнд 2</label>-->
+    <!--    </div>-->
   </div>
 </template>
 
@@ -40,17 +36,7 @@ export default {
     operand1: 0,
     operand2: 0,
     result: 0,
-    items: [
-      1,
-      2,
-      3,
-      4,
-      5,
-      6,
-      7,
-      8,
-      9
-    ],
+    items: [1, 2, 3, 4, 5, 6, 7, 8, 9],
     checked: false,
     picked: false
   }),
@@ -65,7 +51,7 @@ export default {
         '/': () => operand1 / operand2,
         '^': () => Math.pow(operand1, operand2)
       }
-      const finalResult = (calcOperations[op]())
+      const finalResult = calcOperations[op]()
       this.result = Math.floor(finalResult)
     }
   }
@@ -75,20 +61,20 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 h3 {
-  margin : 40px 0 0;
+  margin: 40px 0 0;
 }
 
 ul {
-  list-style-type : none;
-  padding         : 0;
+  list-style-type: none;
+  padding: 0;
 }
 
 li {
-  display : inline-block;
-  margin  : 0 10px;
+  display: inline-block;
+  margin: 0 10px;
 }
 
 a {
-  color : #42b983;
+  color: #42b983;
 }
 </style>
